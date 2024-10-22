a78c5a205e7d7d81e77da5458229175db2d3b207
fDero <francescodero@outlook.it>
2024-10-23 00:52:31 +0200

    Update package-level documentation

--- src/main/java/io/github/fdero/bits4j/core/package-info.java
+++ src/main/java/io/github/fdero/bits4j/core/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package contains classes that provide the core functionality of the bits4j library.
+ * This package contains classes that provide the core functionalities of the bits4j library.
  * 
  * @since 1.0
  * @version 1.0
