ac32ee8bad6e11685035c1172f99825430493795
fDero <francescodero@outlook.it>
2024-10-22 23:36:40 +0200

    Add package-level documentation

---
+++ src/main/java/io/github/fdero/bits4j/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes that provide the core functionality of the bits4j library.
+ * 
+ * @since 1.0
+ * @version 1.0
+ */
+package io.github.fdero.bits4j.core;
---
+++ src/main/java/io/github/fdero/bits4j/stream/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes that provide a way to input and output bits 
+ * using {@link java.io.InputStream} and {@link java.io.OutputStream}.
+ * 
+ * @since 1.0
+ * @version 1.0
+ */
+package io.github.fdero.bits4j.stream;
